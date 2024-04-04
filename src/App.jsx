e21c9c6adb3ed947424fca357554ca14552539a1
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-04-03 20:34:28 -0500

    change config global

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Formulario } from "./components/Formulario";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Formulario />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import baseAdmin from "../utils/baseAdmin.json";
+import Swal from "sweetalert2";
+
+export function Formulario() {
+  const [Cedula, setCedula] = useState("");
+  const [Contraseña, setContraseña] = useState("");
+
+  let router = useNavigate();
+
+  function procesarFormulario(e) {
+    e.preventDefault();
+
+    let search = baseAdmin.find(function (usuario) {
+      return usuario.documento === Cedula && usuario.contraseña === Contraseña;
+    });
+
+    if (!search) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    } else {
+      router("/home");
+    }
+  }
+
+  return (
+    <>
+      <section className="container">
+        <div className="row justify-content-center text-center">
+          <div className="col-12 col-md-6">
+            <img src="../../src/assets/logo.png" alt="" className="img-fluid" />
+            <form className="border rounded p-4" onSubmit={procesarFormulario}>
+              <h2>Controla tu salud</h2>
+
+              <div className="input-group mb-3 mt-4">
+                <span className="input-group-text" id="basic-addon1">
+                  <i className="bi bi-person-vcard"></i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Numero cedula"
+                  id="identificacion"
+                  value={Cedula}
+                  onChange={(e) => setCedula(e.target.value)}
+                />
+              </div>
+              <div className="input-group mb-3 mt-4">
+                <span className="input-group-text" id="basic-addon1">
+                  <i className="bi bi-file-lock2"></i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Contraseña"
+                  id="contraseña"
+                  value={Contraseña}
+                  onChange={(e) => setContraseña(e.target.value)}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Ingresar
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
