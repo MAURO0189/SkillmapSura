8917f9e3fe9aa329125cc8014e75a35899fdc2a3
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-04-04 16:33:44 -0500

    add new functions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,105 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import basedatosrutas from "../utils/basedatosrutas.json";
+import Swal from "sweetalert2";
 
 function Home() {
+  const [usuario, setUsuario] = useState("");
+  console.log(basedatosrutas);
+
+  const mostrarModalBienvenida = () => {
+    Swal.fire({
+      title: `¡Bienvenido, ${usuario}!`,
+      text: "Gracias por iniciar sesión",
+      icon: "success",
+      confirmButtonText: "Cerrar",
+    });
+  };
+
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link disabled"
-                  href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <h1>Ruta de Aprendizaje</h1>
       <br />
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-          <div className="card">
-            <div className="card-body text-white bg-primary">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+        {basedatosrutas.alternativas_aprendizaje.map((ruta, index) => (
+          <div className="col" key={index}>
+            <div className="card">
+              <div className="card-body text-white bg-primary">
+                <h5 className="card-title">{ruta.nombre}</h5>
+                <p className="card-text">{ruta.descripcion}</p>
+                <p>Tipo: {ruta.tipo}</p>
+                <p>Tiempo: {ruta.tiempo} horas</p>
+                <p>Competencias:</p>
+                <ul>
+                  {ruta.competencias.map((competencia, idx) => (
+                    <li key={idx}>{competencia}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <div className="card-body text-white bg-secondary">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <div className="card-body text-white bg-success">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <div className="card-body text-dark bg-info">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
+      {setUsuario && mostrarModalBienvenida()}
     </>
   );
 }
